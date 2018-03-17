c2a79d4bd46253519532d17d5abf9bbc6c847fff
Felipe Onorio <onorio.cpp@gmail.com>
2018-03-17 02:13:30 -0300

    initial commit

---
+++ notification.py
@@ -0,0 +1,31 @@
+#import Flask from flask
+#import Resource, Api from flask_restful
+#
+#create app using Flask
+#create api using Api
+#
+#create resource class inheriting from Resource
+#define methods returning desired data and status code
+#
+#route resource using api.add_resource(resource_class, 'route')
+#url parameter syntax: /x/y/<type:variable> #/blab/bleb/<string:blub>
+#
+#run app using app.run(port=value)
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+estado = True
+
+class Lembrete(Resource):
+    def get(self):
+        global estado
+        estado = not estado
+        return {"caboclo_esqueceu_de_comer_goiabada": estado }, 200
+
+api.add_resource(Lembrete, '/lembretes')
+
+app.run(port=5000)
